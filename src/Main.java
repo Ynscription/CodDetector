b4780549c0fecffb70cc15242b41f9a0cf813d23
Ynscription <ynscription@gmail.com>
2017-09-14 10:52:02 +0200

    Changed library to relative path

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import View.MainW;
 
 public class Main {	
-
+	
+	//Program that detects the codification of a file
+	
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		MainW mainW = new MainW();
